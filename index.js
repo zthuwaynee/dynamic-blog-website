e7c016af0c2b6685363fa9eb3abc6e80024866e4
zthuwaynee <n01753168@humber.ca>
2025-08-04 23:10:57 -0400

    Fix: Link blog post titles to post.html with ID
    Fix: Link blog post titles to post.html with ID

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const postList = document.getElementById('post-list');
+
 const posts = JSON.parse(localStorage.getItem('posts')) || [];
 
-posts.forEach((post, i) => {
+posts.forEach((post, index) => {
   const div = document.createElement('div');
-  div.innerHTML = `<a href="post.html?id=${i}"><h2>${post.title}</h2></a>`;
+
+  div.innerHTML = `
+    <a href="post.html?id=${index}">
+      <h2>${post.title}</h2>
+      <p>${post.content.slice(0, 100)}...</p>
+    </a>
+    <hr/>
+  `;
+
   postList.appendChild(div);
 });
