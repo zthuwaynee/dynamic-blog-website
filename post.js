ffbd8fa40eb09166b7e268a0edb1120d2602b3f3
zthuwaynee <n01753168@humber.ca>
2025-08-04 22:59:05 -0400

    updated post.js and implemented edit and save functionality
    updated post.js and implemented edit and save functionality

--- post.js
+++ post.js
@@ -3,3 +3,9 @@ const posts = JSON.parse(localStorage.getItem('posts')) || [];
 const post = posts[id];
 document.getElementById('edit-title').value = post.title;
 document.getElementById('edit-content').value = post.content;
+document.getElementById('saveBtn').onclick = () => {
+  posts[id].title = document.getElementById('edit-title').value;
+  posts[id].content = document.getElementById('edit-content').value;
+  localStorage.setItem('posts', JSON.stringify(posts));
+  alert('Saved!');
+};
